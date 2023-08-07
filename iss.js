a46387e8b6ad4be6f5ba36a63bfc5424aa168f2b
Yuli Wang <yuliwang1007@gmail.com>
2023-08-07 00:08:54 -0400

    added return

--- iss.js
+++ iss.js
@@ -4,10 +4,12 @@ const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json',(error, response, body) => {
     if (error) {
       callback(error, null);
+      return;
     }
     if (response.statusCode !== 200) { //conncetion succeed but no IP found
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(new Error(msg), null);
+      return;
     }
 
     const ip = JSON.parse(body).ip; //just to get the value
